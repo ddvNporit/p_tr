cdaec15164cfa7c7d56b93c05a83c8bc14e1c87b
ddv5 <dima.nporit@gmail.com>
2023-02-15 18:48:30 +0300

    добавил классы UserAddresы и UserfielsName

---
+++ useraddress.py
@@ -0,0 +1,11 @@
+class UserAddress:
+    def __init__(self, title, company,  address, home, mobile, work, fax, email, email2):
+        self.title = title
+        self.company = company
+        self.home = home
+        self.address = address
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
---
+++ userfield.py
@@ -0,0 +1,6 @@
+class UserfieldsName:
+    def __init__(self, firstname, middlename, lastname, nickname):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname=nickname
