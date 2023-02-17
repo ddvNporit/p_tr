9185707452f5d5247791bab82e7b258b18283aac
ddv5 <dima.nporit@gmail.com>
2023-02-17 14:22:16 +0300

    lesson 2.1

--- test_add_group.py
+++ test_add_group.py
@@ -18,39 +18,35 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.openHomePage(wd)
         self.login(wd, LoginS(username="admin", password="secret"))
-        # wd.find_element_by_name("searchform").click()
         self.addNewGroup(wd, Group(name="fggf", header="dsfdffd", footer="вавпр"))
-        wd.find_element_by_link_text("groups").click()
         self.logout(wd)
     def test_add_empty_group(self):
         wd = self.wd
-        self.openHomePage(wd)
         self.login(wd, LoginS(username="admin", password="secret"))
-        # wd.find_element_by_name("searchform").click()
         self.addNewGroup(wd, Group(name="", header="", footer=""))
-        wd.find_element_by_link_text("groups").click()
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-
+    # def open_page_group(self, wd):
+    #     wd.find_element_by_link_text("groups").click()
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
     def addNewGroup(self, wd, group):
-        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, login):
+        self.openHomePage(wd)
+
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(login.username)
@@ -58,6 +54,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(login.password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_groups_page(wd)
 
     def openHomePage(self, wd):
         wd.get("http://localhost/addressbook/")
