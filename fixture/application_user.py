aa2d2e0b86dc5f6e577058c585453759faeaed4d
ddv5 <dima.nporit@gmail.com>
2023-02-19 14:18:19 +0300

    lesson 2.5 #6

--- fixture/application_user.py
+++ fixture/application_user.py
@@ -1,50 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
 from fixture.session_user import SessionHelper
+from fixture.userfield import UserFieldsHelper
 class Application_u():
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-    def addNewUser(self, userfield, useraddress):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(userfield.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(userfield.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(userfield.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(userfield.nickname)
-        wd.find_element_by_name("title").send_keys(useraddress.title)
-        wd.find_element_by_name("company").send_keys(useraddress.company)
-        wd.find_element_by_name("address").send_keys(useraddress.address)
-        wd.find_element_by_name("home").send_keys(useraddress.home)
-        wd.find_element_by_name("mobile").send_keys(useraddress.mobile)
-        wd.find_element_by_name("work").send_keys(useraddress.work)
-        wd.find_element_by_name("fax").send_keys(useraddress.fax)
-        wd.find_element_by_name("email").send_keys(useraddress.email)
-        wd.find_element_by_name("email2").send_keys(useraddress.email2)
-        wd.find_element_by_name("email3").send_keys(useraddress.email3)
-        wd.find_element_by_name("homepage").send_keys(useraddress.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(useraddress.bday)
-        wd.find_element_by_xpath("//option[@value='25']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(useraddress.bmonth)
-        wd.find_element_by_xpath("//option[@value='December']").click()
-        wd.find_element_by_name("byear").send_keys(useraddress.byear)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("address2").send_keys(useraddress.address2)
-        wd.find_element_by_name("phone2").send_keys(useraddress.phone2)
-        wd.find_element_by_name("notes").send_keys(useraddress.notes)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("home").click()
+        self.user = UserFieldsHelper(self)
 
     def openHomePage(self):
         wd = self.wd
--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.userfield import UserfieldsName
 from fixture.application_user import Application_u
-from model.useraddress import UserAddress
 import pytest
 
 @pytest.fixture()
@@ -12,10 +11,10 @@ def app(request):
 def test_add_user(app):
     app.openHomePage()
     app.session.login(username="admin", password="secret")
-    app.addNewUser(UserfieldsName(firstname="Петров", middlename="Иван", lastname="Иванович", nickname="кличка"), \
-                    UserAddress(title="title", company="comp", address="address", home="home", mobile="232323", work="232344354",\
-                                fax="232323", email="sd@test.com", email2="we@test.net", email3="wewd@t.er", homepage="http://ya.ru",\
-                                bday="23", bmonth="December", byear="1987", address2="jhdufyhdh", phone2="233443", notes="dfdjdj"))
+    app.user.addNewUser(UserfieldsName(firstname="Петров", middlename="Иван", lastname="Иванович", nickname="кличка", \
+                        title="title", company="comp", address="address", home="home", mobile="232323", work="232344354",\
+                        fax="232323", email="sd@test.com", email2="we@test.net", email3="wewd@t.er", homepage="http://ya.ru",\
+                        bday="23", bmonth="December", byear="1987", address2="jhdufyhdh", phone2="233443", notes="dfdjdj"))
     app.session.logout()
 
 
