637aebcb93731fa99ab5db5290a4a9a81f578acb
ddv5 <dima.nporit@gmail.com>
2023-02-19 19:47:47 +0300

    Lesson 2.4 added delete group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from fixture.application import Application
-import pytest
 
-@pytest.fixture()
-def app(request):
-    fixture =Application()
-    request.addfinalizer(fixture.destoy)
-    return fixture
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="fggf", header="dsfdffd", footer="вавпр"))
