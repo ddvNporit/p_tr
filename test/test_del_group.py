d84147c84d806acac1b173b275e2a9b5d951dc45
ddv5 <dima.nporit@gmail.com>
2023-02-19 21:53:09 +0300

    task6 для оценки

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,7 +3,3 @@ def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.delete_first_group()
     app.session.logout()
-def test_delete_first_group_e(app):
-    app.session.login(username="admin", password="secret")
-    app.group.delete_first_group()
-    app.session.logout()
