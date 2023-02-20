844a6798b86604036a3a3d1ed7111bcc2dff71b3
ddv5 <dima.nporit@gmail.com>
2023-02-20 19:44:54 +0300

    Задание №7 на оценку

--- fixture/group.py
+++ fixture/group.py
@@ -17,12 +17,22 @@ class GroupHelper():
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
-    def delete_first_group(self):
+    def delete_group(self):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+    def edit(self, group):
+        wd = self.app.wd
+        self.app.group.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
