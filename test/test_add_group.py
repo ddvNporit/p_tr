106d32b5e681ab0ae43d92a5742536f1ab3361a2
ddv5 <dima.nporit@gmail.com>
2023-02-21 08:57:07 +0300

    task7 try 2

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,11 +3,13 @@ from model.group import Group
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.open_groups_page()
+    app.group.fill(Group(name="", header="", footer=""))
     app.session.logout()
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="fggf", header="dsfdffd", footer="вавпр"))
+    app.group.open_groups_page()
+    app.group.fill(Group(name="test add group name", header="test add group header", footer="test add group footer"))
     app.session.logout()
 
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,5 +3,6 @@ from model.group import Group
 
 def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.fill(Group(name="fdfdwedsds", header="sdssssdd", footer="вавпапапа"))
+    app.group.open_groups_page()
+    app.group.fill(Group(name="test_edit_name", header="test_edit_header", footer="test_edit_footer"))
     app.session.logout()
