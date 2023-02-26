a83116f455c6b9ddd1277175b929da3eb91642f2
ddv5 <dima.nporit@gmail.com>
2023-02-26 12:32:19 +0300

    lesson 3.2

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,14 +5,14 @@ def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
     app.group.new_group()
-    app.group.fill(Group(name="", header="", footer=""))
+    app.group.fill_group_form(Group(name="", header="", footer=""))
     app.group.add_group_submit()
     app.session.logout()
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
     app.group.new_group()
-    app.group.fill(Group(name="test add group name", header="test add group header", footer="test add group footer"))
+    app.group.fill_group_form(Group(name="test add group name", header="test add group header", footer="test add group footer"))
     app.group.add_group_submit()
     app.session.logout()
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,6 +5,6 @@ def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
     app.group.edit_group()
-    app.group.fill(Group(name="test_edit_name", header="test_edit_header", footer="test_edit_footer"))
+    app.group.fill_group_form(Group(name="test_edit_name", header="test_edit_header", footer="test_edit_footer"))
     app.group.edit_group_submit()
     app.session.logout()
