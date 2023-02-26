5fcb16a11435072713afc93086ea8c984f7937a8
ddv5 <dima.nporit@gmail.com>
2023-02-26 17:47:10 +0300

    task9 #1

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,8 +1,14 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test name"))
     app.group.modify_first_group(Group(name="New group name"))
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test name"))
     app.group.modify_first_group(Group(header="Group new header"))
 def test_modify_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test name"))
     app.group.modify_first_group(Group(footer="Group new footer"))
