0f63b0a7dc0e1462e22f14cddf52a81ed04e793c
ddv5 <dima.nporit@gmail.com>
2023-02-26 19:35:02 +0300

    lesson 3.8

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -8,7 +8,7 @@ def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test name"))
     app.group.modify_first_group(Group(header="Group new header"))
-def test_modify_group_footer(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test name"))
-    app.group.modify_first_group(Group(footer="Group new footer"))
+# def test_modify_group_footer(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test name"))
+#     app.group.modify_first_group(Group(footer="Group new footer"))
