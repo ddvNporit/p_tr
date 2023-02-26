b0ae22379d02fe8e61c4bf3284091d221c98437f
ddv5 <dima.nporit@gmail.com>
2023-02-26 18:51:52 +0300

    task9 refactoring test_delete_user.py #3

--- fixture/user.py
+++ fixture/user.py
@@ -92,3 +92,7 @@ class UserFieldsHelper():
         wd = self.app.wd
         wd.find_element_by_xpath("//div[@id='content']/form/input[22]").click()
         wd.find_element_by_link_text("home").click()
+    def count(self):
+        wd = self.app.wd
+        self.open_users_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_delete_user.py
+++ test/test_delete_user.py
@@ -1,7 +1,11 @@
 from model.userfield import UserfieldsName
 
+
 def test_delete_first_user(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test name"))
+    if app.user.count() == 0:
+        app.user.create(UserfieldsName(firstname="Петров", middlename="Иван", lastname="Иванович", nickname="кличка", \
+                                title="title", company="comp", address="address", home_phone="home", mobile_phone="232323", work_phone="232344354", \
+                                fax="232323", email="sd@test.com", email2="we@test.net", email3="wewd@t.er", homepage="http://ya.ru", \
+                                bday="23", bmonth="December", byear="1987", address2="jhdufyhdh", phone2="233443", notes="test add dfdjdj"))
     app.user.delete_user()
 
