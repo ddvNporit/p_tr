a901ad7e121b0a55efa9c0fb92aa80299faa7498
ddv5 <dima.nporit@gmail.com>
2023-02-26 17:59:52 +0300

    task9 deleted test_edit_group.py

--- test/test_edit_group.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from model.group import Group
-
-def test_edit_first_group(app):
-    app.group.open_groups_page()
-    app.group.edit_group()
-    app.group.fill_group_form(Group(name="test_edit_name", header="test_edit_header", footer="test_edit_footer"))
-    app.group.edit_group_submit()
