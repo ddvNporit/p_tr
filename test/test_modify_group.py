380dc61964d164dc3a3a7df7a51d9d2416e34f99
ddv5 <dima.nporit@gmail.com>
2023-03-03 16:36:29 +0300

    lesson 4.8

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,11 +3,17 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test name"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New group name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test name"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="Group new header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 # def test_modify_group_footer(app):
 #     if app.group.count() == 0:
 #         app.group.create(Group(name="test name"))
