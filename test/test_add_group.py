e9796d4ee2dd5f37bea0326401ee49d3d4ba5980
ddv5 <dima.nporit@gmail.com>
2023-03-12 20:27:11 +0300

    lesson 5.7 added random data #1

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,8 @@ import pytest, random, string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10
+    # symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    symbols = string.ascii_letters + string.digits +  " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
