e00f82011b3e275ea66fefd628ea21c7b20a2239
ddv5 <dima.nporit@gmail.com>
2023-03-12 17:47:50 +0300

    task12  проверка на идентичность телефонов true

--- test/test_some_kind_of_contact.py
+++ test/test_some_kind_of_contact.py
@@ -18,7 +18,6 @@ def test_of_any_contact(app):
     index = randrange(len(old_users))
     user.id = old_users[index].id
     contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
