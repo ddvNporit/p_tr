1527c06a77dee6ec8263a20a5ff68dc4bcce65df
ddv5 <dima.nporit@gmail.com>
2023-03-13 22:05:47 +0300

    task 16 на оценку #1

--- conftest.py
+++ conftest.py
@@ -9,13 +9,14 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--baseUrl")
+    password = request.config.getoption("--pass")
     if fixture is None:
         fixture = Application(browser=browser, base_url=base_url)
         # fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password=password)
     return fixture
 
 
@@ -34,3 +35,4 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--pass", action="store", default="secret") # можно default удалить, ноя для удобства не стал
