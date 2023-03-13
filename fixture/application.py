d25c44c587c86a8114eb3e25cb1428b215ed76bb
ddv5 <dima.nporit@gmail.com>
2023-03-13 21:52:53 +0300

    task 16

--- fixture/application.py
+++ fixture/application.py
@@ -4,8 +4,9 @@ from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import UserFieldsHelper
 
+
 class Application:
-    def __init__(self, browser="firefox"):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -18,6 +19,8 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = UserFieldsHelper(self)
+        self.base_url = base_url
+
     def is_valid(self):
         try:
             self.wd.current_url
@@ -27,8 +30,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destoy(self):
         self.wd.quit()
-
