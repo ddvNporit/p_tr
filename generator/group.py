44baae467fa72b3f2f82bc1ea228d12e073590a6
ddv5 <dima.nporit@gmail.com>
2023-03-17 17:53:15 +0300

    lesson 6.12

--- generator/group.py
+++ generator/group.py
@@ -1,4 +1,4 @@
-import random, string, os, json, getopt, sys
+import random, string, os, jsonpickle, getopt, sys
 from model.group import Group
 
 try:
@@ -7,8 +7,8 @@ except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
-n = 5
-f = "data/group.json"
+n = 2
+f = "data/groups.json"
 for o, a in opts:
     if o == "-n":
         n = int(a)
@@ -29,4 +29,5 @@ testdata = [Group(name="", header="", footer="")] + [
 ]
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
