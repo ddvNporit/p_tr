92225b9a806a61fc67c26e982dc1f64386e90ecc
ddv5 <dima.nporit@gmail.com>
2023-03-17 18:30:18 +0300

    added generator data contact

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,26 +1,17 @@
+# -*- coding: utf-8 -*-
 from model.group import Group
 from random import randrange
 
-def test_modify_group_name(app):
+
+def test_modify_group_name(app, json_groups):
     if app.group.count() == 0:
         app.group.create(Group(name="test name"))
     old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
-    group = Group(name="New group name")
+    group = json_groups
     group.id = old_groups[index].id
     app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-# def test_modify_group_header(app):
-#     if app.group.count() == 0:
-#         app.group.create(Group(name="test name"))
-#     old_groups = app.group.get_group_list()
-#     app.group.modify_first_group(Group(header="Group new header"))
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
-# def test_modify_group_footer(app):
-#     if app.group.count() == 0:
-#         app.group.create(Group(name="test name"))
-#     app.group.modify_first_group(Group(footer="Group new footer"))
