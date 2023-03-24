dce8fe9798801337a24e679b197be5b009c5ed12
ddv5 <dima.nporit@gmail.com>
2023-03-24 14:28:30 +0300

    lesson7.4

--- fixture/group.py
+++ fixture/group.py
@@ -73,6 +73,10 @@ class GroupHelper():
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
     def open_groups_page(self):
         wd = self.app.wd
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
@@ -85,6 +89,16 @@ class GroupHelper():
         wd = self.app.wd
         self.open_groups_page()
         self.select_group_by_index(index)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
