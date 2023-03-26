3a19d6f813660539e22a6b84272134d0a9bb74f6
ddv5 <dima.nporit@gmail.com>
2023-03-26 13:07:17 +0300

    task20 готово

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -7,21 +7,27 @@ def test_modify_contact(app, db, json_contact, check_ui):
     if app.contact.count() == 0:
         app.contact.create(contact)
     if check_ui:
-        old_contact = app.contact.get_contact_list()
-        index = randrange(len(old_contact))
-        contact.id = old_contact[index].id
+        old_contacts = app.contact.get_contact_list()
+        index = randrange(len(old_contacts))
+        contact.id = old_contacts[index].id
         app.contact.modify_contact_by_index(index, contact, check_ui)
         new_contacts = app.contact.get_contact_list()
-        old_contact[index] = contact
-        assert len(old_contact) == len(new_contacts)
-        assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+        old_contacts[index] = contact
+        assert len(old_contacts) == len(new_contacts)
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     else:
-        old_contact = db.get_contact_list()
-        db_contact_selected = random.choice(old_contact)
+        old_contacts = db.get_contact_list()
+        db_contact_selected = random.choice(old_contacts)
         contact.id = db_contact_selected.id
         app.contact.modify_contact_by_index(db_contact_selected.id, contact, check_ui)
         new_contacts = db.get_contact_list()
-        assert len(old_contact) == len(new_contacts)
-        assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+        i = 0
+        while i < len(old_contacts):
+            if old_contacts[i].id == db_contact_selected.id:
+                old_contacts[i].firstname = contact.firstname
+                old_contacts[i].header = contact.lastname
+            i += 1
+        assert len(old_contacts) == len(new_contacts)
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
